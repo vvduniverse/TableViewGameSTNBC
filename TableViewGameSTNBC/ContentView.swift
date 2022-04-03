068728b26899f31fdb525ef1d3c5c80dd3adaa52
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-03 14:53:44 +0300

    finished

--- TableViewGameSTNBC/ContentView.swift
+++ TableViewGameSTNBC/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    
+    @State private var count = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RadialGradient(
+                colors: [Color("ColorGInner"), Color("ColorGOutter")],
+                center: .center,
+                startRadius: 5,
+                endRadius: 500)
+            .ignoresSafeArea()
+            
+            TabView(selection: $count) {
+                Rectangle()
+                    .foregroundColor(.red)
+                    .tag(1)
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .tag(2)
+                Rectangle()
+                    .foregroundColor(.mint)
+                    .tag(3)
+                Rectangle()
+                    .foregroundColor(.orange)
+                    .tag(4)
+                Rectangle()
+                    .foregroundColor(.green)
+                    .tag(5)
+                Rectangle()
+                    .foregroundColor(.brown)
+                    .tag(6)
+            }
+            .frame(height: 250)
+            .tabViewStyle(PageTabViewStyle())
+        }
+        .onReceive(timer) { _ in
+            withAnimation(.easeInOut(duration: 1.0)) {
+                count = count == 6 ? 1 : count + 1
+            }
+        }
     }
 }
 
