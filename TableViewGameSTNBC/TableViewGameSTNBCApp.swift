c64b77ec192165dd65b6b348578a5e2f11ae2316
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-03 14:46:59 +0300

    Initial Commit

---
+++ TableViewGameSTNBC/TableViewGameSTNBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewGameSTNBCApp.swift
+//  TableViewGameSTNBC
+//
+//  Created by Vahtee Boo on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TableViewGameSTNBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
